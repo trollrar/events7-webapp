0c74f6fb85db6ecf1c06a482a291f5523c661292
fares <troll.rar@gmail.com>
2023-12-22 19:09:04 +0100

    add event details

--- src/app/main/event/event.component.ts
+++ src/app/main/event/event.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Event} from "./event.interface";
+import {EventService} from "./event.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-event',
   templateUrl: './event.component.html',
   styleUrl: './event.component.scss'
 })
-export class EventComponent {
+export class EventComponent implements OnInit{
+  public event?: Event
+  public notFound = false
 
+  constructor(protected eventService: EventService, private route: ActivatedRoute,) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      const postId = Number(params.get('id'));
+      if (!postId) {
+        this.showNotFound();
+        return;
+      }
+
+      this.loadEvent(postId);
+    });
+  }
+
+  private loadEvent(id: number): void {
+    this.eventService.getOne(id).subscribe(event => {
+        this.event = event;
+      }
+    );
+  }
+
+  private showNotFound(): void {
+    this.notFound = true;
+  }
 }
